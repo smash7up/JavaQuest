deffb06dda16f383f8875cdb53c4d12b3d8ec503
LEGRAND Dylan (SNCF VOYAGEURS / DIRECTION DE LIGNE ALSACE / UO TRACTION DL ALSACE) <9919157t@commun.ad.sncf.fr>
2022-11-07 10:55:34 +0100

    quest 3 ok

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        String movieName = "Indiana Jones et la dernière croisade";
+        boolean isWatch = true;
+        int releaseDate = 1989;
+        double imdbNote = 8.2;
+
+        System.out.println(movieName);
+        System.out.println(isWatch);
+        System.out.println(releaseDate);
+        System.out.println(imdbNote);
+
+    }
+}
